ccac4d95b4986d6767db4708ba9d1885d9c1cb1b
Matthew Baggett <matthew@baggett.me>
2013-10-19 19:38:42 +0100

    fails to enumerate

--- DozerControlBoard/Software/DozerController.asm
+++ DozerControlBoard/Software/DozerController.asm
@@ -1,38 +1,87 @@
 
+_USB_Init_Desc:
+
+;usbdsc.c,161 :: 		void USB_Init_Desc(){
+;usbdsc.c,162 :: 		USB_config_dsc_ptr[0] = &configDescriptor1;
+	MOVLW       _configDescriptor1+0
+	MOVWF       _USB_config_dsc_ptr+0 
+	MOVLW       hi_addr(_configDescriptor1+0)
+	MOVWF       _USB_config_dsc_ptr+1 
+	MOVLW       higher_addr(_configDescriptor1+0)
+	MOVWF       _USB_config_dsc_ptr+2 
+;usbdsc.c,163 :: 		USB_string_dsc_ptr[0] = (const char*)&strd1;
+	MOVLW       _strd1+0
+	MOVWF       _USB_string_dsc_ptr+0 
+	MOVLW       hi_addr(_strd1+0)
+	MOVWF       _USB_string_dsc_ptr+1 
+	MOVLW       higher_addr(_strd1+0)
+	MOVWF       _USB_string_dsc_ptr+2 
+;usbdsc.c,164 :: 		USB_string_dsc_ptr[1] = (const char*)&strd2;
+	MOVLW       _strd2+0
+	MOVWF       _USB_string_dsc_ptr+3 
+	MOVLW       hi_addr(_strd2+0)
+	MOVWF       _USB_string_dsc_ptr+4 
+	MOVLW       higher_addr(_strd2+0)
+	MOVWF       _USB_string_dsc_ptr+5 
+;usbdsc.c,165 :: 		USB_string_dsc_ptr[2] = (const char*)&strd3;
+	MOVLW       _strd3+0
+	MOVWF       _USB_string_dsc_ptr+6 
+	MOVLW       hi_addr(_strd3+0)
+	MOVWF       _USB_string_dsc_ptr+7 
+	MOVLW       higher_addr(_strd3+0)
+	MOVWF       _USB_string_dsc_ptr+8 
+;usbdsc.c,166 :: 		}
+L_end_USB_Init_Desc:
+	RETURN      0
+; end of _USB_Init_Desc
+
 _main:
 
-;DozerController.c,1 :: 		void main() {
-;DozerController.c,2 :: 		PORTD = 0;
+;DozerController.c,14 :: 		void main(void){
+;DozerController.c,15 :: 		ADCON1 |= 0x0F;                         // Configure all ports with analog function as digital
+	MOVLW       15
+	IORWF       ADCON1+0, 1 
+;DozerController.c,16 :: 		CMCON  |= 7;                            // Disable comparators
+	MOVLW       7
+	IORWF       CMCON+0, 1 
+;DozerController.c,17 :: 		PORTD = 0;
 	CLRF        PORTD+0 
-;DozerController.c,3 :: 		TRISD = 0;
+;DozerController.c,18 :: 		TRISD = 0;
 	CLRF        TRISD+0 
-;DozerController.c,4 :: 		while(1){
+;DozerController.c,19 :: 		HID_Enable(&readbuff,&writebuff);       // Enable HID communication
+	MOVLW       _readbuff+0
+	MOVWF       FARG_HID_Enable_readbuff+0 
+	MOVLW       hi_addr(_readbuff+0)
+	MOVWF       FARG_HID_Enable_readbuff+1 
+	MOVLW       _writebuff+0
+	MOVWF       FARG_HID_Enable_writebuff+0 
+	MOVLW       hi_addr(_writebuff+0)
+	MOVWF       FARG_HID_Enable_writebuff+1 
+	CALL        _HID_Enable+0, 0
+;DozerController.c,21 :: 		for(blink_i= 0 ; blink_i <= 3; blink_i++){
+	CLRF        _blink_i+0 
+	CLRF        _blink_i+1 
 L_main0:
-;DozerController.c,5 :: 		PORTD.F2 = 1;
-	BSF         PORTD+0, 2 
-;DozerController.c,6 :: 		delay_ms(150);
-	MOVLW       2
-	MOVWF       R11, 0
-	MOVLW       231
-	MOVWF       R12, 0
-	MOVLW       0
-	MOVWF       R13, 0
-L_main2:
-	DECFSZ      R13, 1, 1
-	BRA         L_main2
-	DECFSZ      R12, 1, 1
-	BRA         L_main2
-	DECFSZ      R11, 1, 1
-	BRA         L_main2
-	NOP
-;DozerController.c,7 :: 		PORTD.F2 = 0;
+	MOVLW       128
+	MOVWF       R0 
+	MOVLW       128
+	XORWF       _blink_i+1, 0 
+	SUBWF       R0, 0 
+	BTFSS       STATUS+0, 2 
+	GOTO        L__main18
+	MOVF        _blink_i+0, 0 
+	SUBLW       3
+L__main18:
+	BTFSS       STATUS+0, 0 
+	GOTO        L_main1
+;DozerController.c,22 :: 		PORTD.F2 = 0;
 	BCF         PORTD+0, 2 
-;DozerController.c,8 :: 		delay_ms(150);
+;DozerController.c,23 :: 		delay_ms(150);
 	MOVLW       2
 	MOVWF       R11, 0
-	MOVLW       231
+	MOVLW       134
 	MOVWF       R12, 0
-	MOVLW       0
+	MOVLW       153
 	MOVWF       R13, 0
 L_main3:
 	DECFSZ      R13, 1, 1
@@ -41,10 +90,32 @@ L_main3:
 	BRA         L_main3
 	DECFSZ      R11, 1, 1
 	BRA         L_main3
-	NOP
-;DozerController.c,9 :: 		}
+;DozerController.c,24 :: 		PORTD.F2 = 1;
+	BSF         PORTD+0, 2 
+;DozerController.c,25 :: 		delay_ms(150);
+	MOVLW       2
+	MOVWF       R11, 0
+	MOVLW       134
+	MOVWF       R12, 0
+	MOVLW       153
+	MOVWF       R13, 0
+L_main4:
+	DECFSZ      R13, 1, 1
+	BRA         L_main4
+	DECFSZ      R12, 1, 1
+	BRA         L_main4
+	DECFSZ      R11, 1, 1
+	BRA         L_main4
+;DozerController.c,21 :: 		for(blink_i= 0 ; blink_i <= 3; blink_i++){
+	INFSNZ      _blink_i+0, 1 
+	INCF        _blink_i+1, 1 
+;DozerController.c,26 :: 		}
 	GOTO        L_main0
-;DozerController.c,10 :: 		}
+L_main1:
+;DozerController.c,27 :: 		while(1){}
+L_main5:
+	GOTO        L_main5
+;DozerController.c,35 :: 		}
 L_end_main:
 	GOTO        $+0
 ; end of _main
--- DozerControlBoard/Software/DozerController.c
+++ DozerControlBoard/Software/DozerController.c
@@ -1,10 +1,35 @@
-void main() {
-     PORTD = 0;
-     TRISD = 0;
-     while(1){
-        PORTD.F2 = 1;
-        delay_ms(150);
-        PORTD.F2 = 0;
-        delay_ms(150);
-     }
+#include "USBdsc.c"
+
+unsigned char readbuff[64] absolute 0x500;   // Buffers should be in USB RAM, please consult datasheet
+unsigned char writebuff[64] absolute 0x540;
+char cnt;
+char kk;
+int blink_i;
+
+
+//void interrupt(){
+  // USB_Interrupt_Proc();                   // USB servicing is done inside the interrupt
+//}
+
+void main(void){
+  ADCON1 |= 0x0F;                         // Configure all ports with analog function as digital
+  CMCON  |= 7;                            // Disable comparators
+  PORTD = 0;
+  TRISD = 0;
+  HID_Enable(&readbuff,&writebuff);       // Enable HID communication
+  
+  for(blink_i= 0 ; blink_i <= 3; blink_i++){
+    PORTD.F2 = 0;
+    delay_ms(150);
+    PORTD.F2 = 1;
+    delay_ms(150);
+  }
+  while(1){}
+  while(1){
+    while(!HID_Read());
+    for(cnt=0; cnt<64; cnt++){
+      writebuff[cnt] = readbuff[cnt];    while(!HID_Write(&writebuff,64))      ;
+      PORTD.F2 = ~PORTD.F2;
+    }
+  }
 }
